8287d3b53f678a551b7be7287375b806abf1d538
Kelly <kelly_cris19@outlook.com.br>
2023-02-03 18:20:01 -0300

    Lista de exercícios de lógica de programação

---
+++ tresDivisores/tresDivisores/Program.cs
@@ -0,0 +1,23 @@
+﻿
+/*Dado um inteiro n, retorne true se n tiver exatamente três divisores positivos. Caso contrário, retorne false. 
+O inteiro m é um divisor de n, se existe um inteiro k tal que n = k * m.*/ 
+
+int count = 0;
+
+Console.WriteLine("Informe um número sem casas decimais: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+for (int contador = 1; contador <= n; contador++)
+{
+    if (n % contador == 0)
+    {
+        count++;
+    }
+
+    if (count > n)
+    {
+        Console.WriteLine(false);
+    }
+}
+Console.WriteLine(count == 3);
